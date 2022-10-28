495ab884ee2eac720420e0833db9a7d87c325507
Jose Vergara <86685478+verangel95@users.noreply.github.com>
2022-10-28 15:18:31 -0500

    Add files via upload

---
+++ Javascript basico/Ejercicio 2/datatypes.js
@@ -0,0 +1,38 @@
+// mi nombre
+
+let nombre = "Jose";
+
+// mi edad
+
+let edad = 27;
+
+// eres desarrollador?
+
+let pregunta = true;
+
+// fecha de nacimiento
+
+let fecha = new Date("02/06/1995");
+
+let dia = fecha.getDate();
+
+let mes = fecha.getMonth() + 1;
+
+let year = fecha.getFullYear();
+
+
+console.log(dia,mes,year);
+
+// mi libro favorito
+
+const gameofThrones = {
+    titulo: "A game of thrones",
+    year: "1997",
+    pages: 254
+}
+
+gameofThrones.fecha = "06/06/1996";
+
+gameofThrones.url = "xxxxxxx";
+
+console.log(gameofThrones);
