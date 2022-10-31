88a75b04161680fd121009dc1f7cbd84945585e1
Jose Vergara <86685478+verangel95@users.noreply.github.com>
2022-10-31 14:10:51 -0600

    Add files via upload

---
+++ Javascript basico/Ejercicio 4/Strings.js
@@ -0,0 +1,20 @@
+
+let Nombre= "Jose";
+let Apellido= "Vergara";
+
+let estudiante = ` ${Nombre} ${Apellido}`;
+
+let estudianteMayus = estudiante.toUpperCase;
+let estudianteminus = estudiante.toLowerCase;
+
+let cuenta= estudiante.length;
+
+
+let primeraletra= Nombre[0];
+
+let ultimaletra = Apellido[Apellido.length-1];
+
+
+let sinespacio = estudiante.trim();
+
+console.log(estudiante.includes("Jose"));  
