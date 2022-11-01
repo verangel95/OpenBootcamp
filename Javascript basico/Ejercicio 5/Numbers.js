f01a0b168625786de177bb3a05eec7d95863b77c
Jose Vergara <86685478+verangel95@users.noreply.github.com>
2022-11-01 09:02:36 -0600

    Add files via upload

---
+++ Javascript basico/Ejercicio 5/Numbers.js
@@ -0,0 +1,18 @@
+
+
+let alturacm = 184;
+let alturam = 1.84;
+
+let pesokg = 98.2;
+
+let alturaredup = alturam.toFixed(0); //Altura en metros redondeada hacia arriba;
+
+let pesokgred= pesokg.toPrecision(2); // Peso en kg redondeado hacia abajo
+
+let valormaximo = Number.MAX_SAFE_INTEGER;
+
+let texto = `"El valor maximo que se puede obtener en javasricpt ${valormaximo} + 1" es el valor maximo que se puede obtener en javascript`
+
+
+
+
