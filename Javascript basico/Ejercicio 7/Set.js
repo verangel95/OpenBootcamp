bb0ee54749651d724ebed0bda62c92c4f570eb5c
Jose Vergara <86685478+verangel95@users.noreply.github.com>
2022-11-07 10:30:19 -0600

    Add files via upload

---
+++ Javascript basico/Ejercicio 7/Set.js
@@ -0,0 +1,9 @@
+let conjunto = new Set();
+
+conjunto.add("Jose","Jose");
+
+
+conjunto.add("Javascript");
+
+
+console.log(conjunto);
---
+++ Javascript basico/Ejercicio 7/fechas.js
@@ -0,0 +1,14 @@
+let hoy = new Date();
+
+let nacimiento = new Date(1995,01,06);
+
+let comparacion = hoy > nacimiento;
+
+let mes = nacimiento.getMonth();
+
+let año = nacimiento.getFullYear();
+
+
+
+
+console.log(año);
