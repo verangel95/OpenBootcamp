baac5c078caa408f1c661200cb988e623f6f8911
Jose Vergara <86685478+verangel95@users.noreply.github.com>
2022-11-13 20:26:02 -0600

    Add files via upload

---
+++ Javascript basico/Ejercicio 10/controller.js
@@ -0,0 +1,11 @@
+
+function suma(a,b){
+    return a+b;
+}
+
+function multiplica(a,b){
+    return a*b;
+}
+
+
+export{ suma,multiplica};
---
+++ Javascript basico/Ejercicio 10/index.js
@@ -0,0 +1,7 @@
+
+import {suma,multiplica} from "./controller.js"
+import chalk from 'chalk';
+
+
+console.log(chalk.green(multiplica(suma(1,2),suma(4,5))));
+
