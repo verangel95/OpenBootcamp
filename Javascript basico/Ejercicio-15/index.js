a60eb5d1025067c915325d043b179d03ec59c7f5
Jose Vergara <86685478+verangel95@users.noreply.github.com>
2022-11-16 12:56:43 -0600

    Add files via upload

---
+++ Javascript basico/Ejercicio-15/index.js
@@ -0,0 +1,11 @@
+const nombre = "Jose";
+const apellido = "vergara";
+
+const objeto = {nombre: nombre, apellido:apellido};
+
+
+// sessionStorage.setItem("informacion",objeto);
+
+// localStorage.setItem("informacion",objeto);
+
+// document.cookie = "mi-primera-cookie,expires=" +  new Date(2022,10,17).toUTCStrings;
